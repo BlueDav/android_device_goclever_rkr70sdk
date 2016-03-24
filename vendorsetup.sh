a3979a9ab7890456002b028f0a5cb1716ba2e7a0
BlueDav <bluedav000@gmail.com>
2016-03-24 17:12:44 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_rkr70sdk-userdebug
-add_lunch_combo full_rkr70sdk-userdebug
+add_lunch_combo cm_rk2928-userdebug
+add_lunch_combo full_rk2928-userdebug
